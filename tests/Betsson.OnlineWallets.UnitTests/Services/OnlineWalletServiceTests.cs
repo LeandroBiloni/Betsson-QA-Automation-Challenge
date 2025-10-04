e78835a44b647270ed7808046dcf6b8fa4aa94ca
LeandroBiloni <leandrobiloni@hotmail.com>
2025-10-04 20:49:45 -0300

    Add test for GetBalance after funds deposit
    Added test for GetBalance function from OnlineWalletsService.
    
    The test verifies that the function correctly returns the current amount the wallet has when funds were added previously.

--- tests/Betsson.OnlineWallets.UnitTests/Services/OnlineWalletServiceTests.cs
+++ tests/Betsson.OnlineWallets.UnitTests/Services/OnlineWalletServiceTests.cs
@@ -4,6 +4,7 @@ using Betsson.OnlineWallets.Data.Repositories;
 using Betsson.OnlineWallets.Services;
 using Betsson.OnlineWallets.Data.Models;
 using Xunit.Abstractions;
+using Betsson.OnlineWallets.Models;
 
 namespace Betsson.OnlineWallets.UnitTests;
 
@@ -40,5 +41,52 @@ public class OnlineWalletsServiceTests
 
         //Assert
         Assert.Equal(expectedBalanceAmount, balance.Amount);
+    }
+
+    [Fact]
+    public async Task GetBalance_AfterFundsDeposit_ReturnsFunds()
+    {
+        //Arrange
+        Deposit deposit = new Deposit { Amount = 10 };
+
+        Mock<IOnlineWalletRepository> mockWalletRepository = new Mock<IOnlineWalletRepository>();
+
+        //This will be the "last entry" created when depositing funds.
+        OnlineWalletEntry lastWalletEntry = new OnlineWalletEntry();
+
+        decimal expectedBalanceAmount = deposit.Amount + lastWalletEntry.Amount;
+
+        mockWalletRepository
+            .Setup(repo => repo.GetLastOnlineWalletEntryAsync())
+            .ReturnsAsync(lastWalletEntry);
+
+        //Stores the deposited funds as lastWalletEntry.
+        mockWalletRepository
+            .Setup(repo => repo.InsertOnlineWalletEntryAsync(It.IsAny<OnlineWalletEntry>()))
+            .Callback<OnlineWalletEntry>(entry =>
+            {
+                lastWalletEntry.Amount = entry.Amount;
+                lastWalletEntry.BalanceBefore = entry.BalanceBefore;
+            })
+            .Returns(Task.CompletedTask);
+
+
+        IOnlineWalletRepository repository = mockWalletRepository.Object;
+
+        OnlineWalletService service = new OnlineWalletService(repository);
+        
+        Balance updatedBalance = await service.DepositFundsAsync(deposit);
+
+        _output.WriteLine("Updated balance: " + updatedBalance.Amount);
+
+
+        //Act
+        Balance balanceAfterDeposit = await service.GetBalanceAsync();
+
+        _output.WriteLine("Balance after deposit: " + balanceAfterDeposit.Amount);
+
+
+        //Assert
+        Assert.Equal(expectedBalanceAmount, balanceAfterDeposit.Amount);
     }
 }
